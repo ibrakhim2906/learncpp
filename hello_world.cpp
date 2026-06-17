f60837ccc89e7eec5eba0a5ed7dfd3dbf57629bf
ibrakhim2906 <ibrakhim2906@yahoo.com>
2026-06-17 13:10:15 +0500

    ...

---
+++ hello_world.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+int main() {
+  std::cout << "Hello, World"; // Everything from here to the end of the line is
+                               // ignored
+                               /*
+                                * Ibrakhim Kopzhassar
+                                * Wrote this program
+                                * Via help of the learncpp.com resource
+                                */
+  return 0;
+}
